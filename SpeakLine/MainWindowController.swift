484981ae0a7ec3e2552e28eba4d6679193150d78
DNwin <ndennis@gmail.com>
2015-07-23 23:35:54 -0700

    Added tableView that allows different voice selection

--- SpeakLine/MainWindowController.swift
+++ SpeakLine/MainWindowController.swift
@@ -9,14 +9,15 @@
 import Cocoa
 
 class MainWindowController: NSWindowController, NSSpeechSynthesizerDelegate,
-                            NSWindowDelegate {
+                            NSWindowDelegate, NSTableViewDelegate, NSTableViewDataSource {
     
     @IBOutlet weak var textField: NSTextField!
     @IBOutlet weak var speakButton: NSButton!
     @IBOutlet weak var stopButton: NSButton!
+    @IBOutlet weak var tableView: NSTableView!
     
     let speechSynth = NSSpeechSynthesizer()
-    
+    let voices = NSSpeechSynthesizer.availableVoices() as! [String]
     // State of speechSynth
     var isStarted: Bool = false {
         didSet {
@@ -32,6 +33,24 @@ class MainWindowController: NSWindowController, NSSpeechSynthesizerDelegate,
         super.windowDidLoad()
         updateButtons()
         speechSynth.delegate = self
+        
+        
+        // Print all voices
+        for voice in voices {
+            let voiceName = voiceNameForIdentifier(voice)
+            println(voiceName)
+        }
+        
+        // Preselect default voice in table
+        let defaultVoice = NSSpeechSynthesizer.defaultVoice()
+        // Search for index of default voice in voices array
+        if let defaultRow = find(voices, defaultVoice) {
+            tableView.scrollRowToVisible(defaultRow)
+            // Convert int to NSIndexSet then set selection
+            let indices = NSIndexSet(index: defaultRow)
+            tableView.selectRowIndexes(indices, byExtendingSelection: false)
+
+        }
     }
     
     // MARK: - Action Methods
@@ -63,6 +82,15 @@ class MainWindowController: NSWindowController, NSSpeechSynthesizerDelegate,
             stopButton.enabled = false
         }
     }
+
+    // Takes in voice identifier from voices array and returns simple string attribute name
+    func voiceNameForIdentifier(identifier: String) -> String? {
+        if let attributes = NSSpeechSynthesizer.attributesForVoice(identifier) {
+            return attributes[NSVoiceName] as? String
+        } else {
+            return nil
+        }
+    }
     
     // MARK: - NSSpeechSynthesizerDelegate
     func speechSynthesizer(sender: NSSpeechSynthesizer, didFinishSpeaking finishedSpeaking: Bool) {
@@ -77,4 +105,36 @@ class MainWindowController: NSWindowController, NSSpeechSynthesizerDelegate,
         // Only allows closing if speech synthesizer is not started
         return !isStarted
     }
+    
+    // MARK: - NSTableViewDataSource
+    
+    // Set number of rows to total number of different voices
+    func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+        return voices.count
+    }
+    
+    func tableView(tableView: NSTableView, objectValueForTableColumn tableColumn: NSTableColumn?, row: Int) -> AnyObject? {
+        let voice = voices[row]
+        let voiceName = voiceNameForIdentifier(voice)
+        
+        return voiceName
+    }
+    
+    // MARK: - NSTableViewDelegate
+    
+    func tableViewSelectionDidChange(notification: NSNotification) {
+        let row = tableView.selectedRow
+        
+        // Set default if no row is selected
+        if (row == -1) {
+            speechSynth.setVoice(nil)
+            return
+        }
+        
+        let voice = voices[row]
+        speechSynth.setVoice(voice)
+    }
+    
+    
+
 }
